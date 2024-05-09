dc5f6bf2eee7d2eb5c0babd6057e703ef1b248e1
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-09 12:52:23 -0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -4,5 +4,6 @@ $dbname = 'calendar';
 $username = 'your_username';
 $password = 'your_password';
 //connect to the database
+//create a new PHP Data Objects object
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 ?>
