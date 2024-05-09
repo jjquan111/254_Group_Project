683e97ec95f661b1adf53098b6a80c9ad0e0589d
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-09 09:04:25 -0700

    Update dynamic_full_calendar.php

--- dynamic_full_calendar.php
+++ dynamic_full_calendar.php
@@ -2,23 +2,36 @@
 <html>
 <head>
    <title>Dynamic Full Calendar</title>
-   <!--link for FullCalendar and Bootstrap-->
+   <!--link for FullCalendar CSS-->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css" rel="stylesheet" />
-   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-   <!-- Linking jQuery, Popper.js, and Bootstrap JS -->
+   <!-- Linking jQuery -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
    <!-- Linking Moment.js and FullCalendar JS -->
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
 </head>
 <body>
+
+<style>
+.add_event {
+      padding: 10px 20px;
+      background-color: #9290C3;
+      color: #ffffff;
+      border: none;
+      font-size: 16px;
+      border-radius: 5px;
+    }
+.add_event:hover {
+      background-color: #535C91;
+    }
+</style>
+
     <!--calendar container-->
    <div id="calendar"></div>
 
 
    <!--add event button-->
-   <button id="addEventBtn" class="btn btn-primary">Add Event</button>
+   <button id="addEventBtn" class="add_event">Add Event</button>
 
 
    <script>
